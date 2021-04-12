4e7c5301ef40c1d2563cb0defe2943e19c354639
Paul Ayo Oloyede <ayopaulofficial@gmail.com>
2021-04-12 07:11:39 +0100

    add navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,6 @@
-
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 import '../scss/theme.scss';
 
-/* Your JS Code goes here */
-
 /* Demo JS */
 import './demo.js';
-
-function goToHome() {
-   
-    console.log(window.host);
-    console.log(window.href)
-}
-
-$(document).ready(function() {
-    console.log('We are ready!')
-    goToHome()
-});
