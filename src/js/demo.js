a0eba289a6b6a45547f5f8b9fcdc28f89618e7d7
Paul Ayo Oloyede <ayopaulofficial@gmail.com>
2021-04-14 06:57:24 +0100

    building each component

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,3 +2,33 @@ const demo = () => 'Webpack Boilerplate v5.5.0 - SASS/PostCSS, ES6/7, browser sy
 
 // eslint-disable-next-line no-console
 console.log(demo());
+
+
+
+$(document).ready(function () {
+    console.log('loading-------')
+    $('.bar--times').show();
+    var trigger = $('button[data-toggle=side-nav]'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.on('click', function () {
+        $('.side-nav').collapse();
+        sideNav(isClosed);      
+    });
+
+})
+
+function sideNav(isClosed) {
+    
+    isClosed = !isClosed;
+    if (isClosed == true) {          
+      $('.bar--icon').hide()
+      $('.bar--times').removeClass('d-none');
+    } else {   
+        $('.bar--icon').show()
+        $('.bar--times').addClass('d-none');
+      
+    }
+}
+
